c716af86ba0cc0fee487f97024ce1c507040acb0
michel475 <michaelchacal776@gmail.com>
2026-02-08 23:17:08 -0300

    Arquivo contendo os imports e credenciais no banco Neo4J.

---
+++ imports/imports_credentials.py
@@ -0,0 +1,6 @@
+from neo4j import GraphDatabase
+from neo4j_graphrag.retrievers import HybridCypherRetriever
+
+uri = "neo4j+s://67429cb5.databases.neo4j.io"
+user = "neo4j"
+password = "GWyg5RoUOwh8JgBluxQ4CtT7xfpcaw9DHsHcM216gYk"
